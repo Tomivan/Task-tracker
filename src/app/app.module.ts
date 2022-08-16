f41950d1752b6481ffd2186cf304b3fabadc71c2
Tomivan <gracejunkie20@gmail.com>
2022-08-16 22:22:24 +0100

    created components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddFormComponent } from './components/add-form/add-form.component';
+import { ButtonComponent } from './components/button/button.component';
+import { HeaderComponent } from './components/header/header.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddFormComponent,
+    ButtonComponent,
+    HeaderComponent,
+    TaskItemComponent
   ],
   imports: [
     BrowserModule,
